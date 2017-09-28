5ddd907cd5b4f2a382708415c57f38bec777c9ac
丁敬 <dingjing@live.cn>
2017-09-28 16:23:37 +0800

    常见图形绘制函数了解

---
+++ base_test7.R
@@ -0,0 +1,31 @@
+rm(list = ls())
+# 基本图形
+#
+# 条形图，展现了类别型变量的分布（频数）
+# barplot()
+# barplot(counts, main, xlab, ylab) 数据、标题、x轴标注、y轴标注
+# title() 和调用绘图函数时候指定 main 都可以设置标题
+#
+# 棘状图(特殊的条形图)
+# spine()
+#
+# 饼图
+# pie(x, labels)
+#
+# 直方图
+# hist(x)
+#
+# 核密度图
+# plot(density(x))
+#
+# 箱线图
+# boxplot()
+#
+# 小提琴图
+# library(vioplot)
+# vioplot()
+#
+# 点图
+# dotchart(x, labels=)
+#
+#
